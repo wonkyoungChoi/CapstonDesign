193ab9811db5bdf14ac651127cdb8631bd390934
wonkyoungChoi <72801820+wonkyoungChoi@users.noreply.github.com>
2021-10-06 12:27:31 +0900

    자취왕 Task 코드 정리

--- app/src/main/java/com/example/capstondesign/model/GroupBuyingCountTask.java
+++ app/src/main/java/com/example/capstondesign/model/GroupBuyingCountTask.java
@@ -2,50 +2,20 @@ package com.example.capstondesign.model;
 
 import android.os.AsyncTask;
 import android.os.Build;
-import android.util.Log;
-
 import androidx.annotation.RequiresApi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class GroupBuyingCountTask extends AsyncTask<String, Void, String> {
-    String sendMsg, receiveMsg;
+    String sendMsg, url;
+    Task task = new Task();
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected String doInBackground(String... strings) {
-        try {
-            String str;
-            URL url = new URL("http://13.124.75.92:8080/GroupBuyingCount.jsp");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestMethod("POST");
-            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-            sendMsg = "nick="+strings[0]+"&title="+strings[1]+"&fileLength="+strings[2];
-            osw.write(sendMsg);
-            osw.flush();
-            if(conn.getResponseCode() == conn.HTTP_OK) {
-                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8);
-                BufferedReader reader = new BufferedReader(tmp);
-                StringBuffer buffer = new StringBuffer();
-                while ((str = reader.readLine()) != null) {
-                    buffer.append(str);
-                }
-                receiveMsg = buffer.toString();
+        url = "http://13.124.75.92:8080/GroupBuyingCount.jsp";
+        sendMsg = "nick="+strings[0]+"&title="+strings[1]+"&fileLength="+strings[2];
 
-            } else {
-                Log.i("통신 결과", conn.getResponseCode()+"에러");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return receiveMsg;
+        return task.start(url, sendMsg);
     }
 
     public String substringBetween(String str, String open, String close) {
--- app/src/main/java/com/example/capstondesign/model/SignUpTask.java
+++ app/src/main/java/com/example/capstondesign/model/SignUpTask.java
@@ -20,39 +20,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class SignUpTask extends AsyncTask<String, Void, String> {
-    String sendMsg, receiveMsg;
+    Task task = new Task();
     @Override
     protected String doInBackground(String... strings) {
-        try {
-            String str;
-            URL url = new URL("http://13.124.75.92:8080/sign_up.jsp");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestMethod("POST");
-            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-            sendMsg = "name="+strings[0]+"&phone_num="+strings[1]+"&email_front="+strings[2]+"&email_end="+strings[3]+"&nick="+strings[4]
-                    +"&pwd="+strings[5] +"&gender="+strings[6];
-            osw.write(sendMsg);
-            osw.flush();
-            if(conn.getResponseCode() == conn.HTTP_OK) {
-                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                BufferedReader reader = new BufferedReader(tmp);
-                StringBuffer buffer = new StringBuffer();
-                while ((str = reader.readLine()) != null) {
-                    buffer.append(str);
-                }
-                receiveMsg = buffer.toString();
-
-            } else {
-                Log.i("통신 결과", conn.getResponseCode()+"에러");
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return receiveMsg;
+            return task.start("http://13.124.75.92:8080/sign_up.jsp",
+                    "name="+strings[0]+"&phone_num="+strings[1]+"&email_front="+strings[2]+"&email_end="+strings[3]
+                            +"&nick="+strings[4] +"&pwd="+strings[5] +"&gender="+strings[6]);
     }
 
     public static class DuplicateCheck {
